ab6818a79d3e93c1d9c89f5042a65352710becbd
Joshua <sandejos@oregonstate.edu>
2022-03-09 16:23:44 -0800

    commit plz

---
+++ WarGame/WarGame.js
@@ -0,0 +1,188 @@
+//Class declares an unshuffled default deck of playing cards - Begin
+class deckOfCards {
+    constructor() {
+      this.cardDeck = [];
+      const cardSuits = [
+        "clubs \u2663",
+        "diamonds \u2666",
+        "hearts \u2665",
+        "spades \u2660",
+      ];
+      const cardRanks = [
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "J",
+        "Q",
+        "K",
+        "A",
+      ];
+      const cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  
+      //For in loop to iterate through each card. Each Suit/Rank of a card has a value of 1-13.
+      for (let cardSuit in cardSuits) {
+        for (let cardRank in cardRanks) {
+          this.cardDeck.push([
+            cardValues[cardRank],
+            cardRanks[cardRank] + " of " + cardSuits[cardSuit],
+          ]);
+        }
+      }
+    }
+  }
+  
+  //Class declares the players for the game - Begin
+  class Players {
+    constructor() {
+      this.player = []; // Player 1 and Player 2 Array
+      this.playerHands = []; // Player 1 and Player 2 Hands to hold cards.
+    }
+  
+    //Function to create players for the game
+    createPlayers() {
+      for (let i = 0; i < 2; i++) {
+        let playerName = prompt(`The name for Player ${[i + 1]}`);
+        this.player.push(playerName);
+      }
+      return this.player;
+    }
+  }
+  
+  //Declares the players to be created for the game - Begin
+  let startPlayers = new Players();
+  let gamePlayers = startPlayers.createPlayers();
+  
+  let Player1 = gamePlayers[0]; // Declares Player 1 for the game.
+  let Player2 = gamePlayers[1]; // Declares Player 2 for the game.
+  
+  //Display who player 1 and player 2 is by name.
+  console.log("Player 1 is:", Player1);
+  console.log("Player 2 is:", Player2);
+  
+  //Declares the deck of cards to be used in the game - Begin
+  alert("Click OK to get a deck of cards.");
+  let newDeckOfCards = new deckOfCards();
+  let gameDeckOfCards = newDeckOfCards.cardDeck;
+  //Logs card values and each card in the deck.
+  
+  //Function to Shuffle the deck of cards - Begin
+  function shuffleDeckOfCards(gameDeckOfCards) {
+    for (
+      let indexOfCards = gameDeckOfCards.length - 1;
+      indexOfCards > 0;
+      indexOfCards--
+    ) {
+      let indexOfShuffledCards = Math.floor(Math.random() * (indexOfCards + 1));
+      let shuffledGameDeckOfCards = gameDeckOfCards[indexOfCards];
+      gameDeckOfCards[indexOfCards] = gameDeckOfCards[indexOfShuffledCards];
+      gameDeckOfCards[indexOfShuffledCards] = shuffledGameDeckOfCards;
+    }
+    return gameDeckOfCards;
+  }
+  
+  //Declares the shuffled game deck of cards - Begin
+  alert("Click OK shuffle your deck of cards.");
+  let shuffledGameDeckOfCards = shuffleDeckOfCards(
+    gameDeckOfCards,
+    newDeckOfCards.cardValues
+  );
+  
+  //Function to equally deal the cards to each player - Begin
+  function dealCards(shuffledGameDeckOfCards) {
+    let half = Math.ceil(shuffledGameDeckOfCards.length / 2);
+    let player1Hands = shuffledGameDeckOfCards.slice(0, half);
+    let player2Hands = shuffledGameDeckOfCards.slice(-half);
+  
+    return [player1Hands, player2Hands];
+  }
+  
+  //Declares what each player hand will be - Begin
+  alert("Click OK to deal the cards to each player.");
+  let playerHands = dealCards(shuffledGameDeckOfCards);
+  let player1Hands = playerHands[0]; // Player 1 Card Hand
+  let player2Hands = playerHands[1]; // Player 2 Card Hand
+  alert(`\n Let the WAR begin! \n`);
+  
+  //Determines each turn for the game - Begin
+  function takeTurns(player1HandArr, player2HandArr, Player1, Player2) {
+    let player1Score = 0; // Player 1 Score
+    let player2Score = 0; // Player 1 Score
+  
+    for (let i = 0; i < player1HandArr.length; i++) {
+      if (player1HandArr[i][0] < player2HandArr[i][0]) {
+        console.log(
+          "\n",
+          Player1,
+          "Loses:",
+          player1HandArr[i][1],
+          "-VS-",
+          Player2,
+          "Wins:",
+          player2HandArr[i][1]
+        );
+        player2Score += 1; // Adds 1 Point for Player 2
+        console.log(Player1, "gives a point to", Player2, ":", player2Score);
+      } else if (player1HandArr[i][0] > player2HandArr[i][0]) {
+        console.table(
+          "\n",
+          Player2,
+          "Loses:",
+          player2HandArr[i][1],
+          "-VS-",
+          Player1,
+          "Wins:",
+          player1HandArr[i][1]
+        );
+  
+        player1Score += 1; // Adds 1 point for Player 1
+        console.table(Player2, "gives a point to", Player1, ":", player1Score);
+      } else if (player1HandArr[i][0] === player2HandArr[i][0]) {
+        console.table(
+          // No players are rewards a point.
+          `
+          Player Hands are equal: We are going to card WAR! Both players lose! :("\n
+          ${Player1}: ${player1HandArr[i][1]} vs ${Player2}: ${player2HandArr[i][1]}`
+        );
+        player1HandArr.shift();
+        player2HandArr.shift();
+      }
+    }
+    return [player1Score, player2Score];
+  }
+  
+  //Declares the player turns totals - Begin
+  let finalScore = takeTurns(player1Hands, player2Hands, Player1, Player2);
+  
+  //Function to count and output the player score and who won. - Begin
+  function countPlayerScore(finalScore, player1name, player2name) {
+    if (finalScore[0] > finalScore[1]) {
+      console.log(`\n
+      Game Over!
+      ${player1name}'s score is: ${finalScore[0]}
+      ${player2name}'s score is: ${finalScore[1]}
+      ${player1name} Wins!
+      `);
+    } else if (finalScore[0] < finalScore[1]) {
+      console.log(`\n
+      Game Over!
+      ${player1name}'s score is: ${finalScore[0]}
+      ${player2name}'s score is: ${finalScore[1]}
+      ${player2name} Wins!
+      `);
+    } else {
+      `\n
+      Game Over!
+      ${player1name}'s score is: ${finalScore[0]}
+      ${player2name}'s score is: ${finalScore[1]}
+      The game was a draw.
+    `;
+    }
+  }
+  
+  console.log(countPlayerScore(finalScore, Player1, Player2));
--- week1Assignment/ConditionsAndLoops.js
+++ week1Assignment/ConditionsAndLoops.js
@@ -1,16 +1,134 @@
-var customerNames = [];
-customerNames.push('Mike Hawk');
-customerNames.push('ben dover');
+class PlayerOne{
+  constructor(name, points){
+  this.name = name;
+  this.points = points;
+  }
+}
+class PlayerTwo{
+  constructor(name, points){
+    this.name = name;
+    this.points = points;
+  }
+}
 
-console.log(customerNames[0]);
-let i = 0;
-function penis(){
-do{
+class WarGameMainMenu{
+  constructor(){
+      this.names = [];
+      this.names2 = [];
+  }
+
+  StartWarMainMenu(){
+    let selection = this.showMainMenu();
+
+    while(selection != 0){
+      switch(selection){
+        case '1':
+          this.createPlayerOne();
+          break;
+        case '2':
+          this.createPlayerTwo();
+          break;
+        case '3':
+          this.startAGameOfWar();
+          break;
+        default:
+          return 0;
+      }
+      selection = this.showMainMenu();
+    }
+
+  }
+
+  showMainMenu(){
+    return prompt(`
+    1. Create Player One
+    2. Create Player Two
+    3. Play War
+    All Other Buttons Will Terminate the Program`);
+  }
+  
+  createPlayerOne(){
+    let name = prompt('Please enter a name for Player One: ');
+    this.names.push(new PlayerOne(name));
+    console.log(name);
+  }
+
+  createPlayerTwo(){
+    let name = prompt('Please enter a name for Player Two:');
+    this.names2.push(new PlayerTwo(name));
+    console.log(name);
+  }
+
+  startAGameOfWar(){
+   
+   function getDeck(){ const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J','Q','K'];
+
+    let deck = [];
+
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < values.length; j++) {
+      let card = { Value: values[j], Suit: suits[i] };
+      deck.push(card);
+        }
+      }
+      return deck;
+    }
     
-    console.log(i)
-    i++;
-} while(i<=100){
+    let deck = getDeck();
+    
+    function randomizer(){
+    for (let i = deck.length - 1; i > 0; i--) {
+      let k = Math.floor(Math.random() * i);
+      let temp = deck[i];
+      deck[i] = deck[k];
+      deck[k] = temp;
+    }
+    return deck;
+  }
 
-}}
+   let deckrandom = randomizer();
 
-penis();
+   function dealCards(){
+          let hand = [];
+
+          while(hand.length < 52){
+            hand.push(deckrandom.pop());
+          }
+          return hand;
+                    
+        }
+          let hand = dealCards();
+
+          const half = Math.ceil(hand.length / 2);
+          let playerOneCards = hand.slice(0, half)
+          let playerTwoCards = hand.slice(-half)
+        
+      
+      console.log(playerOneCards);
+      console.log(playerTwoCards);
+      
+
+      function playGame(playerOneCards,playerTwoCards, PlayerOne, PlayerTwo) {
+        let playerOneScore = 0;
+        let playerTwoScore = 0;
+
+        for(let i = 0; i < 26; i++){
+          if(playerOneCards > playerTwoCards){
+            console.log('Player one wins');
+            playerOneScore +=1;
+          } else if (playerTwoCards > playerOneCards){
+            console.log('Player two wins');
+            playerTwoScore +=1;
+          } else (playerOneCards == playerTwoCards)
+          console.log('No Points Given');
+        }
+      }
+      playGame();
+  
+      
+  }
+}
+
+let menu = new WarGameMainMenu();
+menu.StartWarMainMenu();
